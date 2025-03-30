6721b99a388a3ff2f92212f10d2a8735dee145f3
malcolm-connolly <46220712+malcolm-connolly@users.noreply.github.com>
2025-03-30 17:47:41 +0100

    Update __init__.py

--- value_iteration/__init__.py
+++ value_iteration/__init__.py
@@ -1,4 +1,5 @@
-from .value_iteration import value_iteration, Markov_Decision_Process
+from .value_iteration import value_iteration
+from .Markov_Decision_Process import Markov_Decision_Process
 
 __version__ = '1.0.0'
 __author__ = 'Malcolm Connolly'
